4ce89e6e68b0414d6cb483ea0811c9ec9d815ae4
wzc0x0 <wzc0x0@gmial.com>
2018-03-05 18:46:16 +0800

    add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
+import { Switch, Route, Link } from 'react-router-dom';
 import logo from './assets/img/react.png'
 
 import './css/app.css'
+import asyncComponent from './js/asyncComponent'
+
+const Home = asyncComponent(() => import('./components/home'))
+const Detail = asyncComponent(() => import('./components/detail'))
 
 class App extends Component {
     constructor(props) {
-        super(props) 
-        
-        this.state = { 
-            counter: 0 
+        super(props)
+
+        this.state = {
+            counter: 0
         };
     }
 
@@ -18,10 +23,19 @@ class App extends Component {
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
                     <h1 className="App-title">Don't be afraid of React!</h1>
+                    <div>
+                      <span><Link to='/'>home</Link></span>
+                      <span><Link to='detail'>detail</Link></span>
+                    </div>
                 </header>
-                <p className="App-intro" onClick={() => this.setState({ counter : 2})}>
+                {/* <p className="App-intro" onClick={() => this.setState({ counter : 2})}>
                      To get started, edit <code>src/App.js</code> and save to reload. {this.state.counter}
-                </p>
+                </p> */}
+                <Switch>
+                  <Route exact path='/' component={Home}></Route>
+                  <Route exact path='/detail' component={Detail}></Route>
+                </Switch>
+
             </div>
         )
     }
---
+++ src/components/detail.jsx
@@ -0,0 +1,14 @@
+/* import React , { Component } from "react";
+
+export default class Detail extends Component {
+  render() {
+    return (
+      <h1>Hello!This is the detail page!</h1>
+    )
+  }
+}
+ */
+import React from 'react'
+
+const Detail = () => <h1>Hello!This is the detail page!</h1>
+export default Detail
