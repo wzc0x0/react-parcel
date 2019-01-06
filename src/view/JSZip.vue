f558f4c0a7332432b7d66c1b7daeda2840200a44
wzc0x0 <wangzichen@peilian.com>
2019-01-06 17:37:45 +0800

    add cpu usage detect

--- src/view/JSZip.vue
+++ src/view/JSZip.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
     <button @click="download">download</button>
+    <h1>CPU Usage:{{cpuData}}%</h1>
   </div>
 </template>
 <script>
 const JSZip = require("jszip/dist/jszip.min");
 const saveAs = require("jszip/vendor/FileSaver");
-const downloader = url =>
+
+const _ = require("lodash");
+/* const downloader = url =>
   new Promise((resolve, reject) => {
     fetch(url).then(res => {
       if (res.status === 200 || res.status === 0) {
@@ -19,21 +22,22 @@ const downloader = url =>
         });
       } else reject(new Error(res.statusText));
     });
-  });
+  }); */
 
 class DownloaderUpdateFile {
   constructor(JSZip) {
     this.jszipInstance = new JSZip();
   }
 
-  downloader(url, fileName = DownloaderUpdateFile.generateRandomFileName()) {
+  downloader(url, fileName) {
+    fileName = fileName || DownloaderUpdateFile.generateRandomFileName();
     return new Promise(resolve => {
       fetch(url).then(res => {
         if (res.status === 200 || res.status === 0) {
           res.headers.forEach((v, k) => {
             if (k.toString().toLowerCase() === "content-type")
               this.jszipInstance.file(
-                `${fileName}.${v.split("/")[1]}`,
+                `${fileName}.${v.match(/\/\w+/g)[0].slice(1)}`,
                 res.blob(),
                 { binary: true }
               );
@@ -59,20 +63,61 @@ class DownloaderUpdateFile {
     return random + "-" + time;
   }
 }
+const detectUsageOfCPU = cb => {
+  let data,
+    t,
+    pulse = () => {
+      t && (data = Math.abs(Date.now() - t - 50));
+      t = Date.now();
+      setTimeout(pulse, 50);
+      typeof cb === "function" && cb(data);
+    };
+  pulse();
+};
 
 export default {
+  data() {
+    return {
+      cpuData: null,
+      assets: [
+        {
+          url:
+            "https://vips-static.pnlyy.com/wk_app_group/4b5b7b505731f06d62d445ea76b87d53",
+          fileName: "1"
+        },
+        {
+          url: "https://vips-static.pnlyy.com/lvF3h0zPHSDDKnee9r5lwFnAdxxp",
+          fileName: "2"
+        },
+        {
+          url:
+            "https://tests001.pnlyy.com/linewechatclass/7bbc274da20563655d937ba8f0d1abef",
+          fileName: "3"
+        }
+      ]
+    };
+  },
   methods: {
     download() {
-      let url =
-        "https://tests001.pnlyy.com/linewechatclass/7bbc274da20563655d937ba8f0d1abef";
       let instance = new DownloaderUpdateFile(JSZip);
-      let array = [url].map(item => instance.downloader(item));
-      Promise.all([...array]).then(() => {
+      let promiseArray = this.assets.map(item =>
+        instance.downloader(item.url, item.fileName)
+      );
+      Promise.all([...promiseArray]).then(() => {
         instance.jszipInstance
-          .generateAsync({ type: "blob" }, metaData => {
-            console.log(metaData.percent);
-            console.log(metaData.currentFile);
-          })
+          .generateAsync(
+            {
+              type: "blob",
+              compression: "DEFLATE",
+              compressionOptions: {
+                level: 9
+              }
+            },
+            metaData => {
+              console.log(metaData.percent);
+              console.log(metaData.currentFile);
+            }
+          )
           .then(
             blob => {
               saveAs(blob, "sb.zip");
@@ -82,26 +127,13 @@ export default {
             }
           );
       });
-      /* let jszip = new JSZip(),
-        url =
-          "https://tests001.pnlyy.com/linewechatclass/7bbc274da20563655d937ba8f0d1abef";
-      downloader(url).then(({ blob, extension }) => {
-        jszip.file(`11111.${extension}`, blob, { binary: true });
-        jszip
-          .generateAsync({ type: "blob" }, metaData => {
-            console.log(metaData.percent);
-            console.log(metaData.currentFile);
-          })
-          .then(
-            blob => {
-              saveAs(blob, "111sb.zip");
-            },
-            err => {
-              console.log(err);
-            }
-          );
-      }); */
+    },
+    showCPU(data) {
+      this.cpuData = data;
     }
+  },
+  created() {
+    detectUsageOfCPU(_.throttle(this.showCPU, 1000));
   }
 };
 </script>
