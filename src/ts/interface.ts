e171de53b3741df4b74eb8d32b584f445de72ae6
wzc0x0 <wangzichen@peilian.com>
2019-02-19 19:14:44 +0800

    add

--- src/ts/interface.ts
+++ src/ts/interface.ts
@@ -8,3 +8,47 @@ function printLable(value: LabelValue) {
 
 let myObj = { size: 10, label: "Size 10 Object" };
 printLable(myObj);
+
+interface SquareConfig {
+  color?:string
+  width?:number
+  [propName:string]:any
+}
+
+function print(v:SquareConfig) {
+  console.log(v)
+}
+
+print({color:"#f0f",width:100,height:100})
+
+
+interface SearchFunc {
+  (source:string,subString:string):boolean
+}
+
+let MySearch:SearchFunc
+
+MySearch = function (src,sub) {
+  let res = src.search(sub)
+  return res > -1
+}
+
+interface StringArray {
+  [index:number]:string
+}
+
+let myArray:StringArray = ["Bob","Alan"]
+
+interface ClockInterface {
+  currentTime:Date
+  setTime(d:Date)
+}
+
+class Clock implements ClockInterface {
+  currentTime:Date;
+  setTime(d:Date) {
+    this.currentTime = d
+  }
+  constructor(h:number,m:number) {
+  }
+}
